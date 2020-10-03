0e9ff4dfb64508945652e79e2a93844d17a50f1c
ushimaru <uxsxmxr14@gmail.com>
2020-10-03 23:59:55 +0900

    画面モックconfig

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+  ini_set('display_errors',1);
+  require_once(__DIR__ . '/define.php');
+  require_once(__DIR__ .'/../lib/Controller/functions.php');
+  require_once(__DIR__ . '/autoload.php');
+  session_start();
+  //urlFilter();
+
+
---
+++ config/define.php
@@ -0,0 +1,5 @@
+<?php
+define('DSN','mysql:host=localhost;charset=utf8;dbname=');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/original_appli/public_html');
