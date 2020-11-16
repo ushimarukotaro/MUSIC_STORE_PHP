d0d6c1dd970e9bcfed3781c894fdd192fe621f4e
ushimaru <uxsxmxr14@gmail.com>
2020-11-16 18:35:51 +0900

    minor_change_1116

--- public_html/cart_delete_all.php
+++ public_html/cart_delete_all.php
@@ -3,10 +3,10 @@ session_start();
 $session_name = session_name();
 $_SESSION['cart'] = array();
 $_SESSION['num'] = array();
-// unset($_SESSION['cart']);
+
 if (isset($_COOKIE[$session_name]) === true) {
   setcookie($session_name, '', time() - 3600);
 }
-// session_destroy();
+
 header('Location:cart_list.php');
 exit;
