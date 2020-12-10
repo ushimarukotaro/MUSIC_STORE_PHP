9bb45a057a4dd01846423b290214b0fce101974e
ushimaru <uxsxmxr14@gmail.com>
2020-12-10 17:17:36 +0900

    changed_histories_style

--- lib/Model/Product.php
+++ lib/Model/Product.php
@@ -184,6 +184,16 @@ class Product extends \Shop\Model {
     }
   }
 
+
+  //購入履歴数と日付取得
+  public function getHistories() {
+    $stmt = $this->db->prepare("SELECT COUNT(h.id) AS h_id,h.created AS h_created FROM histories AS h WHERE h.user_id = :id GROUP BY h.created ORDER BY h.created DESC LIMIT 10");
+    $stmt->execute([
+      ':id' => $_SESSION['me']->id,
+    ]);
+    return $stmt->fetchAll(\PDO::FETCH_OBJ);
+  }
+
   //　購入履歴表示
   public function showPurchaseHistory($values) {
     $stmt = $this->db->prepare("SELECT p.id AS p_id,h.created AS h_created,h.num AS h_num,p.image,p.product_name,p.maker,p.price FROM products AS p INNER JOIN histories AS h ON p.id = h.product_id WHERE h.user_id = :id AND delflag = 0 ORDER BY h.created DESC LIMIT 10");
--- public_html/purchase_history.php
+++ public_html/purchase_history.php
@@ -2,42 +2,50 @@
 require_once(__DIR__ . '/header.php');
 $showHistories = new Shop\Model\Product();
 $products = $showHistories->showPurchaseHistory($_SESSION['me']->id);
+$histories = $showHistories->getHistories($_SESSION['me']->id);
+// var_dump($histories);
 ?>
 <div class="title">
   <h1 class="page__ttl">購入履歴</h1>
 </div>
-<?php foreach ($products as $product) : ?>
-  <table class="table favorite">
+
+<?php foreach ($histories as $history) : ?>
+  <table class="table favorite hist">
     <tbody>
-      <tr>
-        <th class="purchase-day" colspan="3">
-          <span>購入日：<?= h(substr($product->h_created,0,11)) ?></span>
-          <span>　　購入数：<?= h($product->h_num) ?>点</span>
+      <tr class="hist_first_tr">
+        <th class="purchase-day t_head" colspan="3">
+          <span>購入日：<?= h(substr($history->h_created, 0, 11)) ?></span>
+          <span>　　購入数：<?= h($history->h_id) ?>点</span>
         </th>
       </tr>
-      <tr>
-        <td class="img-area">
-          <form id="product_disp" method="get">
-            <a href="<?= SITE_URL ?>/product_disp.php?id=<?= h($product->p_id) ?>" class="item" onclick="document.getElementById('product_disp').submit"><img src="./gazou/<?= h($product->image); ?>" alt=""></a>
-          </form>
-        </td>
-        <td class="detail-area">
-          <p><?= h($product->maker); ?></p>
-          <p><?= h($product->product_name); ?></p>
-          <p class="err">¥<?= h(number_format($product->price)); ?>（税抜）</p>
-          <p>¥<?= h(number_format(floor($product->price * 1.10))); ?>（税込）</p>
-        </td>
-        <form id="review" method="get" action="<?= SITE_URL ?>/review_post.php?id=<?= h($product->id) ?>">
-          <td>
-            <button class="btn btn-primary" onclick="document.getElementById('review').submit()">レビューを投稿</button>
-            <input type="hidden" name="id" value="<?= h($product->p_id) ?>">
-            <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
-          </td>
-        </form>
-      </tr>
+      <?php foreach ($products as $product) : ?>
+        <?php if ($history->h_created == $product->h_created) : ?>
+          <tr>
+            <td class="img-area">
+              <form id="product_disp" method="get">
+                <a href="<?= SITE_URL ?>/product_disp.php?id=<?= h($product->p_id) ?>" class="item" onclick="document.getElementById('product_disp').submit"><img src="./gazou/<?= h($product->image); ?>" alt=""></a>
+              </form>
+            </td>
+            <td class="detail-area">
+              <p><?= h($product->maker); ?></p>
+              <p><?= h($product->product_name); ?></p>
+              <p class="err">¥<?= h(number_format($product->price)); ?>（税抜）</p>
+              <p>¥<?= h(number_format(floor($product->price * 1.10))); ?>（税込）</p>
+            </td>
+            <form id="review" method="get" action="<?= SITE_URL ?>/review_post.php?id=<?= h($product->id) ?>">
+              <td>
+                <button class="btn btn-primary" onclick="document.getElementById('review').submit()">レビューを投稿</button>
+                <input type="hidden" name="id" value="<?= h($product->p_id) ?>">
+                <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+              </td>
+            </form>
+          </tr>
+          <?php endif; ?>
+        <?php endforeach; ?>
     </tbody>
   </table>
 <?php endforeach; ?>
+
 <a href="product_all.php" class="btn btn-link">商品一覧へ</a>
 
 <?php
